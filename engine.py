b30d4703a590c0be72891f3fa0be73e2ef22273f
nitesh-optionchain <nitesh.kajal@gmail.com>
2026-07-02 13:51:57 +0530

    Update engine.py

--- engine.py
+++ engine.py
@@ -4,12 +4,14 @@ import streamlit as st
 
 @st.cache_resource(show_spinner=False)
 def get_engine():
+    st.write("get_engine started")
     try:
         from nubra_python_sdk.marketdata.market_data import MarketData
         from nubra_python_sdk.start_sdk import InitNubraSdk, NubraEnv
         from nubra_python_sdk.ticker import websocketdata
         
         nubra = InitNubraSdk(NubraEnv.PROD, env_creds=True)
+        st.write("MarketData created")
         
         def on_msg(msg):
             name = msg.get('indexname')
@@ -22,6 +24,7 @@ def get_engine():
         socket.connect()
         socket.subscribe(["NIFTY", "SENSEX", "BANKNIFTY", "INDIAVIX"], data_type="index", exchange="NSE")
         threading.Thread(target=socket.keep_running, daemon=True).start()
+        st.write("MarketData created")
         return MarketData(nubra)
     except: 
         return None

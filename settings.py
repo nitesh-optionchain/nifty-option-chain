e2b7503cda8647a10fb4c233311af1d5abfc9dc9
nitesh-optionchain <nitesh.kajal@gmail.com>
2026-07-04 10:56:15 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,19 +1,4 @@
 # settings.py
-import os
-
-USER_FILE = "authorized_users.json"
-SESSION_FILE = "session_login.json"
-DATA_FILE = "admin_data_v2.json"
-SETTINGS_FILE = "matrix_settings.json"
-
-# --- MERCHANDISE UPI SETTINGS CONFIG ---
-ADMIN_UPI_ID = "9304768496@ybl"  # Yahan aapki UPI ID
-MONTHLY_SUBSCRIPTION_FEES = 799.00     # Fees amount
-
-# Admin Mappings
-ADMIN_DB_DEFAULT = {"9304768496": "Admin Chief", "7982046438": "Admin x"}
-SUPER_ADMIN_IDS = ["9304768496", "7982046438"]
-
 # settings.py (Added Refresh Rate Option)
 import os
 
@@ -24,7 +9,7 @@ SETTINGS_FILE = "matrix_settings.json"
 
 # --- MERCHANDISE UPI SETTINGS CONFIG ---
 ADMIN_UPI_ID = "9304768496@ybl"
-MONTHLY_SUBSCRIPTION_FEES = 499.00
+MONTHLY_SUBSCRIPTION_FEES = 799.00
 
 # Admin Mappings
 ADMIN_DB_DEFAULT = {"9304768496": "Admin Chief", "7982046438": "Admin x"}

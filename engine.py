a234f74f827b00ee74ec4a96e63cc59ef31615af
nitesh-optionchain <nitesh.kajal@gmail.com>
2026-07-04 11:22:01 +0530

    Update engine.py

--- engine.py
+++ engine.py
@@ -2,7 +2,6 @@
 import threading
 import streamlit as st
 
-@st.cache_resource(show_spinner=False)
 def get_engine():
     try:
         from nubra_python_sdk.marketdata.market_data import MarketData
